5fee334aa1a0d4e0eb2a4bd31130d4b8abe1b609
ninhatzo <anac.estudantes2@gmail.com>
2025-09-13 00:50:49 -0300

    Adicionando a data

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
+//CALENDÁRIO
+function mostrarData() {
+    const dataAtual = new Date();
+
+    const diasSemana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+    const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+    const diaSemana = diasSemana[dataAtual.getDay()];
+    const dia = dataAtual.getDate();
+    const mes = meses[dataAtual.getMonth()];
+    const ano = dataAtual.getFullYear();
+
+    const dataFormatada = `${diaSemana}, ${dia} de ${mes} de ${ano}`;
+
+    document.getElementById('data').textContent = dataFormatada;
+}
+
+mostrarData();
+
+// RELÓGIO
 const horas = document.getElementById('horas');
 const minutos = document.getElementById('minutos');
 const segundos = document.getElementById('segundos');
@@ -10,9 +30,9 @@ const relogio = setInterval(function time() {
     
     if (hr < 10) hr = '0' + hr;
 
-    if (min < 10) hr = '0' + min;
+    if (min < 10) min = '0' + min;
 
-    if (secs < 10) hr = '0' + secs;
+    if (secs < 10) secs = '0' + secs;
 
     horas.textContent = hr;
     minutos.textContent = min;
